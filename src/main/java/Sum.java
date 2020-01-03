142a72e2864cd6f70a1d0a54c9eaa00b0aa992c2
rbulin <roman.bulinski@pentacomp.pl>
2020-01-03 20:24:44 +0100

    Implemented code

--- src/main/java/Sum.java
+++ src/main/java/Sum.java
@@ -1,7 +1,15 @@
 public class Sum {
-    public int GetSum(int a, int b)
-    {
-        //Good luck!
-        return 0;
+    public int GetSum(int a, int b) {
+        if(a>b){
+            int temp = a;
+            a = b;
+            b=temp;
+        }
+        int result = 0;
+        while(a<=b){
+            result = result + a;
+            a++;
+        }
+        return result;
     }
 }
--- src/test/java/SumTest.java
+++ src/test/java/SumTest.java
@@ -7,10 +7,13 @@ public class SumTest {
     Sum s = new Sum();
 
     @Test
-    public void Test1()
-    {
+    public void Test1() {
         assertEquals(-1, s.GetSum(0, -1));
         assertEquals(1, s.GetSum(0, 1));
+        assertEquals(0, s.GetSum(-10, 10));
+        assertEquals(  15, s.GetSum(0,5));
+        assertEquals(-5, s.GetSum(-2,-3));
+
     }
 
 }
